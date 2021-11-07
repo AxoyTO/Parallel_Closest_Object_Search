d35b7083a61242edb263c284434aafaa27a48f3a
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2021-11-07 22:36:11 +0300

    Update EARLYBREAK

--- Code/EARLYBREAK.py
+++ Code/EARLYBREAK.py
@@ -1,7 +1,6 @@
 import numpy as np
 import time
 from stl import mesh
-from mpi4py import MPI
 from math import sqrt
 
 
@@ -81,17 +80,9 @@ if __name__ == "__main__":
     print(f"NaiveHDD Result: {NaiveHDD_result}")
     print(f"NaiveHDD Elapsed Time: {NaiveHDD_time :.12f} seconds.")
     start = time.time()
-    stmpi = MPI.Wtime()
-    comm = MPI.COMM_WORLD
-    world_size = comm.Get_size()
-    proc_rank = comm.Get_rank()
     EARLYBREAK_result = EARLYBREAK(tank1_vertices[0:1000], tank2_vertices[0:1000], euclidean)
-    end = time.time()
-    endmpi = MPI.Wtime()
     EARLYBREAK_time = end - start
-    eb_mpitime = endmpi - stmpi
     print(f"EARLYBREAK Result: {EARLYBREAK_result}")
     print(f"EARLYBREAK Elapsed Time: {EARLYBREAK_time:.12f} seconds.")
-    print(f"EARLYBREAK MPI Elapsed Time: {eb_mpitime:.12f} seconds.")
     speedup = NaiveHDD_time / EARLYBREAK_time
     print(f"SPEEDUP: {speedup:.2f}x")
