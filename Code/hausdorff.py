e741b1492eb2448756acdf9a8763712ea306a29d
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2022-05-06 12:01:27 +0300

    Now able to work with high amount of models

--- Code/hausdorff.py
+++ Code/hausdorff.py
@@ -12,3 +12,20 @@ def NaiveHDD(model_a, model_b, distance_function=Metrics.euclidean):
                 dist_min = dist
         distances_list.append(dist_min)
     return np.max(distances_list)
+
+
+def EARLYBREAK(model_a, model_b, distance_function=Metrics.euclidean):
+    nA = model_a.shape[0]
+    nB = model_b.shape[0]
+    cmax = 0.0
+    for i in range(nA):
+        cmin = np.inf
+        for j in range(nB):
+            d = distance_function(model_a[i], model_b[j])
+            if d < cmin:
+                cmin = d
+            if cmin < cmax:
+                break
+        if cmax < cmin:
+            cmax = cmin
+    return cmax
