a2bf49580451a6c9e04ca2e73803b8efd8312e08
Oguz Aksoy <oguzhan.aksoy@1ci.com>
2023-03-05 02:07:47 +0300

    threading asyncio

--- Code/bdspool.py
+++ Code/bdspool.py
@@ -1,10 +1,10 @@
-from copy import copy
 from functools import partial
 import os
-import time
 import random
 import numpy as np
 import trimesh
+import asyncio
+import threading
 from hausdorff import *
 from scipy.spatial.distance import directed_hausdorff
 import mpi4py
@@ -42,6 +42,50 @@ def load_model_by_name(model_name):
                 )
             return np.array(model.vertices)
 
+async def control_requests():
+    print_flushed(f"Process {rank} is ready to receive requests")
+    probe = comm.iprobe(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+    if probe:
+        destination = comm.recv()
+        print_flushed(f"Process {rank} received request from {destination}")
+        if len(models) > 0:
+            model_index = models.index(random.choice(models))
+            model_name = models.pop(model_index)
+            comm.send(model_name, dest=destination, tag=destination)
+
+def receive_model_and_calculate_distance():
+
+    if rank == 0:
+        model_index = models.index(random.choice(models))
+        model_name = models.pop(model_index)
+
+    else:
+        #print_flushed(f"Process {rank} is ready to receive model")
+        model_name = comm.sendrecv(rank, dest=0, source = 0, sendtag=rank, recvtag=rank)
+
+        if model_name is None:
+            return 0
+            
+    calculate_distance(model_name)
+    
+def calculate_distance(model_name):
+        model = load_model_by_name(model_name)
+        results_dict[model_name] = max(directed_hausdorff(fixed_model, model),directed_hausdorff(model, fixed_model))[0]
+        #results_dict[model_name] = max(EARLYBREAK(fixed_model, model),EARLYBREAK(model, fixed_model))
+        print_flushed(f"Process {rank} calculated Hausdorff distance from {fixed_model_name} to {model_name}: {results_dict[model_name]:.6f}")
+
+async def receive_model_and_calculate_distance_for_0():
+    model_index = models.index(random.choice(models))
+    model_name = models.pop(model_index)
+    calculate_distance(model_name)
+    #print_flushed(f"Process {rank} says hi")
+
+
+def async_control():
+    asyncio.run(control_requests())
+
+def async_receive():
+    asyncio.run(receive_model_and_calculate_distance_for_0())
 
 if __name__ == "__main__":
     MPI.Init()
@@ -61,98 +105,64 @@ if __name__ == "__main__":
     models = [i[:-4] for i in files if i.endswith((".stl", ".STL", ".off", ".OFF"))]
 
     if rank == 0:
-
-        if len(models) < world_size:
-            error = 1
-            print_flushed("ERROR: Not enough models to run in parallel.")
-            print_flushed(f"Process count: {world_size} | Model count: {len(models)}")
-            print_flushed(
-                "Process count must be less than or equal to the number of models."
-            )
-        for i in range(1, world_size):
-            comm.send(error, dest=i, tag=i)
-        if error:
-            MPI.Finalize()
-            print_flushed("Exiting...")
-            exit()
-
         print_flushed("==================================")
         print_flushed(f"          WORLD SIZE: {world_size}          ")
         print_flushed("==================================")
         print_flushed(f"Total model count: {len(models)}")
-        print_flushed(models)
+        #print_flushed(models)
 
         fixed_model_index = models.index(
-            random.choice(models)
+            'Darren'
         )  # Pick a random model to be fixed
         print_flushed(f"Process {rank} picked model: {models[fixed_model_index]}.")
         print_flushed("==================================")
 
         start = MPI.Wtime()
+        fixed_model_name = models.pop(fixed_model_index)
+        fixed_model = load_model_by_name(fixed_model_name)
+
         if world_size > 1:
-            fixed_model_name = models.pop(fixed_model_index)
             for i in range(1, world_size):
                 comm.send(fixed_model_name, dest=i, tag=i)  # Send fixed_model to other processes
 
-                #print_flushed(f"Process {rank} sent {fixed_model} to process {i}.")
-                #print_flushed(f"Process {rank} sent {models} to process {i}.")
-            #comm.barrier()
-            
-
     else:
-        for i in range(1, world_size):
-            if rank == i:
-                error = comm.recv(source=0, tag=i)
-                if error == 1:
-                    MPI.Finalize()
-                    exit()
-
         fixed_model_name = comm.recv(source=0) # Receive fixed_model from process 0
         models.pop(models.index(fixed_model_name))
         fixed_model = load_model_by_name(fixed_model_name)
-        #print(rank, fixed_model[:2])
-        #print_flushed(f"Process {rank} received fixed model: {fixed_model}.")
-        #print_flushed(f"Process {rank} has models: {models}.")
+
         
-    #comm.barrier()
-    #MPI.Finalize()
-    while True:
-        # Request model from process 0
+    while True and world_size > 1:
         if rank == 0:
-            if len(models) > 0:
-                #print_flushed(f"Process 0 has {len(models)} models left. {models}")
-                destination = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
-                #print_flushed(f"Process 0 received a request from {destination}")
-                model_index = models.index(random.choice(models))
-                model = models.pop(model_index)
-                comm.send(model, dest=destination, tag=destination)
-                #print_flushed(f"Process 0 sent {model} to {destination}.")
+            if len(models)> 0:
+                t1 = threading.Thread(target=async_control)
+                t1.start()
+                t1.join()
                 
             else:
                 for i in range(1, world_size):
                     comm.send(None, dest=i, tag=i)
                 break
+                
         else:
-            # Send rank to process 0 to request a model
-            comm.send(rank, dest=0, tag=rank)
-            #print_flushed(f"Process {rank} sent a request to 0")
-
-            # Receive model index from process 0
-            model_name = comm.recv(source=0, tag=rank)
-
-            # If there are no more elements left, break out of the loop
-            if model_name is None:
-                #print_flushed(f"Process {rank} received None.")
+            result = receive_model_and_calculate_distance()
+            if result != 0:
+                pass
+            else:
                 break
-            
+
+    if world_size > 1:
+        res = comm.gather(results_dict, root=0)
+    else:
+        res = []
+        model_name = models
+        for model_name in models:
             model = load_model_by_name(model_name)
-            #results_dict[model_name] = max(directed_hausdorff(fixed_model, model),directed_hausdorff(model, fixed_model))[0]
-            results_dict[model_name] = max(EARLYBREAK(fixed_model, model),EARLYBREAK(model, fixed_model))
-            print_flushed(f"Process {rank} calculated Hausdorff distance from {fixed_model_name} to {model_name}: {results_dict[model_name]:.6f}")
-    0
-    #print(f"Process {rank} has results: {results_dict}")
-    
-    res = comm.gather(results_dict, root=0)
+            results_dict[model_name] = max(directed_hausdorff(fixed_model, model),directed_hausdorff(model, fixed_model))[0]
+            #results_dict[model_name] = max(EARLYBREAK(fixed_model, model),EARLYBREAK(model, fixed_model))
+            #print_flushed(f"Process {rank} calculated Hausdorff distance from {fixed_model_name} to {model_name}: {results_dict[model_name]:.6f}")
+        res.append(results_dict)
+        results_dict = {}
+        
     if rank == 0:
         end = MPI.Wtime()
         for i in res:
@@ -167,8 +177,7 @@ if __name__ == "__main__":
     
     MPI.Finalize()
 
-# TODO: change scipy hausdorff to custom hausdorff
-# TODO: для 1 процесса
+
 # TODO: писать текст
 # TODO: больше моделей
 # TODO: запускать на POLUS
